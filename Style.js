ea545f5013a30b05c4c2f972ebd7f872849bbd7c
RanchDevloperx <102632151+RanchDevloperx@users.noreply.github.com>
2022-11-15 08:38:31 +0800

    Add files via upload

---
+++ Style.js
@@ -0,0 +1,9 @@
+let root = document.querySelector('#root');
+let element = document.createElement('h1');
+
+element.textContent = "Cetrekan Lampu Slur";
+element.style.color = 'white';
+element.style.textAlign = 'center';
+element.style.backgroundColor = 'purple';
+element.style.padding = '30px 10px 30px 10px';
+root.appendChild(element)
